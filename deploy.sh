1e43e21011eaafdd5f5230e81d487e211f1d2993
l3nn4rt <l3nn4rt@protonmail.com>
2019-09-23 12:42:24 +0200

    Fix missing gschema compilation

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 NAME=freon@UshakovVasilii_Github.yahoo.com
+glib-compile-schemas $NAME/schemas
 rm -rf ~/.local/share/gnome-shell/extensions/$NAME
 cp -r $NAME ~/.local/share/gnome-shell/extensions/.
