a11b1ea1ded419d63e008f182d437dee5e2f3364
Adam <71682312+duonggiakhanhb@users.noreply.github.com>
2021-10-18 12:36:29 +0700

    fix average and temperature sensors (#214)
    * fix average and temperature sensors
    
    Get length except for any temp equal to 0
    
    * fix average of temperature (liquidctl sensors)

--- freon@UshakovVasilii_Github.yahoo.com/extension.js
+++ freon@UshakovVasilii_Github.yahoo.com/extension.js
@@ -381,7 +381,7 @@ const FreonMenuButton = GObject.registerClass(class Freon_FreonMenuButton extend
             let sum = 0;
             let max = 0;
             for (let i of tempInfo){
-                if(i.temp !== null && i.temp >= 0){
+                if(i.temp !== null && i.temp > 0){
                     total++;
     	            sum += i.temp;
     	            if (i.temp > max)
@@ -424,13 +424,15 @@ const FreonMenuButton = GObject.registerClass(class Freon_FreonMenuButton extend
 
             if(sensorsTempInfo.length > 0 && this._settings.get_boolean('group-temperature')){
                 sum = 0;
+                let sensorsTempLength = 0
                 for (let i of sensorsTempInfo){
                     sum += i.temp;
+                    if(i.temp > 0) sensorsTempLength++;
                 }
                 sensors.push({
                     type:'temperature-group',
                     label:'temperature-group',
-                    value: this._formatTemp(sum / sensorsTempInfo.length)});
+                    value: this._formatTemp(sum / sensorsTempLength)});
             }
 
             for (let fan of fanInfo){
